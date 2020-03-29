9ef98874641b506943549d50af6c5fdde72dfa21
Otávio <otavio.stein@gmail.com>
2020-03-29 17:40:25 -0300

    mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+
+     // View é usada para qualquer tipo de container
+     // Text é usada para qualquer tipo de texto
+     // No react native, não existe herança de estilos
+
+    <Routes />
+  );
+}
